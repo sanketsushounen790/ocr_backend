a6adcf20972a40f26228eeac924e534b38dec3e3
Thai Huy <soisanbongdem999@gmail.com>
2024-06-08 09:45:39 +0700

    update the UI nothing to do much with backend

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.use('/images', express.static('images'));
 
 const fs = require("fs");
 
+// Ham chuyen base64String thanh hinh dinh dang jpg
 const convertBase64StringToJpgImage = async (base64String) => {
-    // Create a base64 string from an image => ztso+Mfuej2mPmLQxgD ...
-    //const base64 = fs.readFileSync("./images.jpg", "base64");
+    
     //console.log(base64String)
+
     // Convert base64 to buffer => <Buffer ff d8 ff db 00 43 00 ...
     const buffer = Buffer.from(base64String, "base64");
 
@@ -41,12 +42,15 @@ app.post('/convert', async (req, res) => {
     const base64String = req.body?.data?.base64String
     //console.log(req.body?.data?.base64String)
 
+    //Chuyen base64String thanh jpg
     await convertBase64StringToJpgImage(base64String)
 
+    //Chuyen tu hinh jpg sang text
     tesseract
         .recognize("./public/images/text_image.jpg", config)
         .then((text) => {
             console.log("Result:", text)
+            //chuyen ve frontend text
             res.send({ text: text })
         })
         .catch((error) => {
