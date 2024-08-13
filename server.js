bdc7d5abd1efd2cc14eafd85019b91046c84d450
thaihuy <thaihuyduong790@gmail.com>
2024-08-13 17:04:54 +0700

    complete demo - waiting for code description

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const vieConfig = {
 
 const enConfig = {
   lang: "eng",
+  psm: 1,
 };
 
 // Use CORS middleware
@@ -38,95 +39,77 @@ app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
-// app.get('/ping-elastic', async (req, res) => {
-//     const book = await elasticClient.get({
-//         index: 'books',
-//         id: '1f43feb0-ac1e-4ab3-b4d3-f9c417162e48',
-//       })
-
-//       console.log(book)
-
-//     res.send("OK !")
-// })
-
-// app.post('/search', async (req, res) => {
-
-//     const base64String = req.body?.data?.base64String
-//     const language = req?.body?.data?.language
-//     //console.log(req.body?.data?.base64String)
-
-//     console.log(base64String)
-
-//     //Chuyen base64String thanh jpg
-//     await convertBase64StringToJpgImage(base64String)
-
-//     //Chuyen tu hinh jpg sang text
-//     if(language === "eng"){
-//         console.log("Chuyen doi theo he tieng Anh")
-//         tesseract
-//         .recognize("./public/images/text_image.jpg", enConfig)
-//         .then((text) => {
-//             console.log("Result:", text)
-//             //chuyen ve frontend text
-//             res.send({ text: text })
-//         })
-//         .catch((error) => {
-//             console.log(error.message)
-//         })
-//     }
-//     else {
-//         console.log("Chuyen doi theo he tieng Viet")
-//         tesseract
-//         .recognize("./public/images/text_image.jpg", vieConfig)
-//         .then((text) => {
-//             console.log("Result:", text)
-//             //chuyen ve frontend text
-//             res.send({ text: text })
-//         })
-//         .catch((error) => {
-//             console.log(error.message)
-//         })
-//     }
-
-// })
-app.get("/e-search", async (req, res) => {
+app.get("/test-search", async (req, res) => {
   console.log("Get the book flow");
   const book = await elasticClient.search({
     index: "books",
     query: {
-      match: {
-        chapter_title: "1008 THE WEALTH OF NATIONS",
+      bool: {
+        should: [
+          {
+            match_phrase: {
+              book_title: "THE WEALTH OF NATIONS",
+            },
+          },
+          {
+            match_phrase: {
+              chapter_title: "THE WEALTH OF NATIONS",
+            },
+          },
+        ],
       },
     },
   });
 
-  console.log(book);
-  res.json(book);
+  console.log(book.hits.hits);
+  //console.log(book.hits.hits[0]);
+  res.json(book.hits.hits);
+});
+
+app.get("/popular-books", async (req, res) => {
+  console.log("Get the book flow");
+  const book = await elasticClient.search({
+    index: "books",
+    size: 21,
+    query: {
+      match_all: {},
+    },
+  });
+
+  console.log(book.hits.hits.length);
+  //console.log(book.hits.hits[0]);
+  res.json({ books: book.hits.hits });
+});
+
+app.post("/search-book-by-text-term", async (req, res) => {
+  console.log("search-book-by-text-term flow");
+  console.log(req.body?.data?.searchTerm);
+  const searchTerm = req.body?.data?.searchTerm;
+  const book = await elasticClient.search({
+    index: "books",
+    size: 21,
+    query: {
+      match_phrase: {
+        book_title: searchTerm,
+      },
+    },
+  });
+
+  console.log(book.hits.hits);
+
+  res.json({ books: book.hits.hits });
 });
 
 app.post("/search", async (req, res) => {
   console.log("Get the book flow");
-  const book = await elasticClient.search({
-    index: "books",
-    query: {
-      match: {
-        chapter_title: "1008 THE WEALTH OF NATIONS",
-      },
-    },
-  });
-
-  console.log(book);
 
   const base64String = req.body?.data?.base64String;
   const language = req?.body?.data?.language;
-  //console.log(req.body?.data?.base64String)
 
-  //console.log(base64String)
-
-  //Chuyen base64String thanh jpg
+  //Chuyển ảnh định dạng base64String thành jpg
   await convertBase64StringToJpgImage(base64String);
 
-  //Chuyen tu hinh jpg sang text
+  //Chuyển ảnh định dạng tu jpg sang text bằng tesseract ocr
   if (language === "eng") {
     console.log("Chuyen doi theo he tieng Anh");
     tesseract
@@ -134,41 +117,54 @@ app.post("/search", async (req, res) => {
       .then(async (text) => {
         console.log("Extract Text Result:");
         console.log(text);
-        //console.log(text.slice(0, text.indexOf("\n")));
 
+        //Cắt bỏ khoảng trống và số khỏi text thu được : dùng text này để search trong elasticsearch
         const extractedChapterTitle = text.slice(0, text.indexOf("\n"));
         const extractedChapterTitleWithouNumber = extractedChapterTitle.replace(
           /[0-9]/g,
           ""
         );
 
+        console.log(extractedChapterTitleWithouNumber);
+
+        //phần text đoạn văn được trả về cho client
         const extractedText = text.slice(text.indexOf("\n"));
-        //console.log(extractedChapterTitleWithouNumber)
 
         const book = await elasticClient.search({
-          index: "books",
+          index: "chapters",
           query: {
-            match: {
-              chapter_title: extractedChapterTitleWithouNumber,
+            bool: {
+              should: [
+                {
+                  match_phrase: {
+                    book_title: extractedChapterTitleWithouNumber,
+                  },
+                },
+                {
+                  match_phrase: {
+                    chapter_title: extractedChapterTitleWithouNumber,
+                  },
+                },
+              ],
             },
           },
         });
 
         console.log(book.hits.hits);
 
-        //Kiem sach chinh xac nhat (highest score)
+        //Kiểm tra sách chính xác nhất (highest score)
         let highestScore = book.hits.hits[0];
 
-        // for (let i = 1; i < book.hits.hits.length; i++) {
-        //   if (book.hits.hits[i]["_score"] > highestScore) {
-        //     highestScore = book.hits.hits[i];
-        //   }
-        // }
+        for (let i = 1; i < book.hits.hits.length; i++) {
+          if (book.hits.hits[i]["_score"] > highestScore) {
+            highestScore = book.hits.hits[i];
+          }
+        }
 
         console.log("Sach chinh xac nhat");
         console.log(highestScore);
 
-        //Chuyen ve frontend
+        //Chuyển dữ liệu về mobile client
         res.json({
           book: highestScore,
           text: extractedText,
